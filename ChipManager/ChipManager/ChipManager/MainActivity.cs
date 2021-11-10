fc3b612a87463855a3f1856a4c4d68215d6446e5
NoamRaveh23 <nnraveh@gmail.com>
2021-11-10 15:36:23 +0200

    add statistics and profile layout

--- ChipManager/ChipManager/ChipManager/MainActivity.cs
+++ ChipManager/ChipManager/ChipManager/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace ChipManager
@@ -8,13 +9,50 @@ namespace ChipManager
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button newGame , conGame, options, stat, profile;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            newGame = (Button)FindViewById(Resource.Id.newGame);
+            conGame = (Button)FindViewById(Resource.Id.ConGame);
+            options = (Button)FindViewById(Resource.Id.options);
+            stat = (Button)FindViewById(Resource.Id.stat);
+            profile = (Button)FindViewById(Resource.Id.profile);
+            newGame.Click += NewGame_Click;
+            conGame.Click += ConGame_Click;
+            options.Click += Options_Click;
+            stat.Click += Stat_Click;
+            profile.Click += Profile_Click;
         }
+
+        private void Profile_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Stat_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Options_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ConGame_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void NewGame_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);

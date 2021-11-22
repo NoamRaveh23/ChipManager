8ea2aa75ff53c48a8197de673c583984d2ce92cc
NoamRaveh23 <nnraveh@gmail.com>
2021-11-22 10:33:43 +0200

    added activities to all screens

--- ChipManager/ChipManager/ChipManager/MainActivity.cs
+++ ChipManager/ChipManager/ChipManager/MainActivity.cs
@@ -1,8 +1,10 @@
 ﻿using Android.App;
+using Android.Media;
 using Android.OS;
 using Android.Runtime;
 using Android.Widget;
 using AndroidX.AppCompat.App;
+using Android.Content;
 
 namespace ChipManager
 {
@@ -10,6 +12,8 @@ namespace ChipManager
     public class MainActivity : AppCompatActivity
     {
         Button newGame , conGame, options, profile;
+        MediaPlayer mp;
+        AudioManager am;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,26 +28,35 @@ namespace ChipManager
             conGame.Click += ConGame_Click;
             options.Click += Options_Click;
             profile.Click += Profile_Click;
+            mp = MediaPlayer.Create(this, Resource.Raw.PokerMusic);
+            mp.Start();
+            am = (AudioManager)GetSystemService(Context.AudioService);
+            int max = am.GetStreamMaxVolume(Stream.Music);
+            am.SetStreamVolume(Stream.Music, max / 2, 0);
         }
 
         private void Profile_Click(object sender, System.EventArgs e)
         {
-            throw new System.NotImplementedException();
+            Intent intent = new Intent(this, typeof(ProfileActivity));
+            StartActivity(intent);
         }
 
         private void Options_Click(object sender, System.EventArgs e)
         {
-            throw new System.NotImplementedException();
+            Intent intent = new Intent(this, typeof(OptionsActivity));
+            StartActivity(intent);
         }
 
         private void ConGame_Click(object sender, System.EventArgs e)
         {
-            throw new System.NotImplementedException();
+            Intent intent = new Intent(this, typeof(GameActivity));
+            StartActivity(intent);
         }
 
         private void NewGame_Click(object sender, System.EventArgs e)
         {
-            throw new System.NotImplementedException();
+            Intent intent = new Intent(this, typeof(StartGameActivity));
+            StartActivity(intent);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ ChipManager/ChipManager/ChipManager/TurnActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChipManager
+{
+    [Activity(Label = "TurnActivity")]
+    public class TurnActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.turn);
+            // Create your application here
+        }
+    }
+}

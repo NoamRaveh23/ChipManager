5faed25c7bf1505dbba7f0b0704b767b3036a7e1
NoamRaveh23 <nnraveh@gmail.com>
2022-01-31 11:16:59 +0200

    firebase startr

---
+++ ChipManager/ChipManager/ChipManager/FirebaseHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Threading.Tasks;
+
+namespace ChipManager
+{
+	class FirebaseHelper
+	{
+        public static FirebaseClient firebase = new FirebaseClient("https://chipmanager-5c652-default-rtdb.europe-west1.firebasedatabase.app/");
+        public static string database = "Games";
+
+        public static async Task<List<Game>> GetAll()
+        {
+            return (await firebase
+              .Child(database)
+              .OnceAsync<Game>()).Select(item => new Game
+              {
+                  state = item.Object.state,
+                  id = item.Object.id
+              }).ToList();
+        }
+
+        public static async Task Add(Game game)
+        {
+
+            await firebase
+              .Child(database)
+              .PostAsync(game);
+        }
+
+        public static async Task<Game> Get(int id)
+        {
+            var allPersons = await GetAll();
+            await firebase
+              .Child(database)
+              .OnceAsync<Game>();
+            return allPersons.Where(a => a.id == id).FirstOrDefault();
+        }
+
+        public static async Task Update(Game state)
+        {
+            var toUpdatePerson = (await firebase
+              .Child(database)
+              .OnceAsync<Game>()).Where(a => a.Object.id == state.id).FirstOrDefault();
+
+            await firebase
+              .Child(database)
+              .Child(toUpdatePerson.Key)
+              .PutAsync(state);
+        }
+
+        public static async Task Delete(int personId)
+        {
+            var toDeletePerson = (await firebase
+              .Child(database)
+              .OnceAsync<Game>()).Where(a => a.Object.id == personId).FirstOrDefault();
+            await firebase.Child(database).Child(toDeletePerson.Key).DeleteAsync();
+
+        }
+    }
+}
---
+++ ChipManager/ChipManager/ChipManager/Game.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChipManager
+{
+    class Game:Player
+    {
+        protected List<Player> game;
+        public int state { get; set; }
+        public int id { get; set; }
+        public Game(List<Player> game)
+        {
+            this.game = game;
+        }
+        public Game()
+        {
+            this.game = null;
+        }
+    }
+}

71be91d509282f415fc47ac0de7d80ad2d46deb4
Noam Raveh23 <nnraveh@gmail.com>
2022-03-06 06:02:07 -0800

    Telephony classes

---
+++ ChipManager/ChipManager/ChipManager/MyPhoneReceiver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Telephony;
+using Android.Views;
+using Android.Widget;
+
+namespace ChipManager
+{
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new[] { "android.intent.action.PHONE_STATE" }, Priority = (int)IntentFilterPriority.HighPriority)]
+    public class MyPhoneReceiver : BroadcastReceiver
+    {
+        public MyPhoneReceiver()
+        {
+        }
+        public override void OnReceive(Context context, Intent intent)
+        {
+            MyPhoneStateListener phoneStateListener = new MyPhoneStateListener();
+            TelephonyManager telephonyManager =
+            (TelephonyManager)Application.Context.GetSystemService(Context.TelephonyService); telephonyManager.Listen(phoneStateListener, PhoneStateListenerFlags.CallState);
+        }
+    }
+}
---
+++ ChipManager/ChipManager/ChipManager/MyPhoneStateListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Telephony;
+using Android.Views;
+using Android.Widget;
+
+namespace ChipManager
+{
+    public class MyPhoneStateListener : PhoneStateListener
+    {
+        public override void OnCallStateChanged(CallState state, string incomingNumber)
+        {
+            base.OnCallStateChanged(state, incomingNumber);
+            switch (state)
+            {
+                case CallState.Ringing:
+                    Toast.MakeText(Application.Context, incomingNumber, ToastLength.Short).Show(); break;
+                case CallState.Offhook:
+                    break;
+                case CallState.Idle:
+                    Toast.MakeText(Application.Context, "idle", ToastLength.Short).Show(); break;
+            }
+        }
+    }
+}
